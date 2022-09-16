40d623dce310007c59a67e6f940f5017a0ccb119
turbokungfu <slashrandazzo@gmail.com>
2022-09-16 13:36:35 -0700

    added comment capabilities

---
+++ routes/comments.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Comment Routes - simplified for now
+
+
+router.post("/createComment/:id", commentsController.createComment);
+
+router.put("/likeComment/:id", commentsController.likeComment);
+
+// router.delete("/deletePost/:id", commentsController.deleteComment);
+
+module.exports = router;
